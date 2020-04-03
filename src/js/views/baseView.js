5f5ecc64d43effc21d287669371b75fc41a52621
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-03 17:25:52 +0900

    WIP-search: eplisis function added

--- src/js/views/baseView.js
+++ src/js/views/baseView.js
@@ -1,6 +1,7 @@
 /* ===========================================
-This file contains following.
-- All DOM strings
+This module contains common variables and functions as follows
+- DOM strings
+- name ellipsis function
 ============================================= */
 
 // -------------- DOMstrings -----------------
@@ -8,4 +9,9 @@ export const dom = {
   searchInput: document.querySelector(".search__field"),
   searchField: document.querySelector(".search"),
   searchResults: document.querySelector("ul.results__list")
+};
+
+export const ellipsis = (name, length) => {
+  if (name.length >= length) return name.substring(0, length) + "...";
+  return name;
 };
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,4 +1,4 @@
-import { dom } from "./baseView";
+import { dom, ellipsis } from "./baseView";
 
 export const getSearchValue = () => dom.searchInput.value;
 
@@ -11,7 +11,7 @@ export const displayResults = results => {
             <img src="${result.image_url}" alt="${result.title}">
           </figure>
           <div class="results__data">
-            <h4 class="results__name">${result.title}</h4>
+            <h4 class="results__name">${ellipsis(result.title, 20)}</h4>
             <p class="results__author">${result.publisher}</p>
           </div>
         </a>
